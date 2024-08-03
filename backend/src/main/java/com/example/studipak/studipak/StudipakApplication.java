ece6a2dd7581c84c71a1eaa42272ffb7d7823b9e
theonlyduckleftinmars <j.herdev1456@gmail.com>
2024-08-03 02:18:32 -0500

    initialize spring application

---
+++ backend/src/main/java/com/example/studipak/studipak/StudipakApplication.java
@@ -0,0 +1,13 @@
+package com.example.studipak.studipak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudipakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudipakApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/studipak/studipak/StudipakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studipak.studipak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudipakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
